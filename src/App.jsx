40219ae31b608fe316797e4f88e0504ffa07cb0e
Andres Caceres <andrescacerescoronel@gmail.com>
2024-10-17 11:44:50 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <main>
+      <Nav />
+    </main>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+    {href:"#home", label:"Home"},
+    {href:"#about-me", label:"About Me"},
+    {href:"#services", label:"Services"},
+    {href:"#projects", label:"Projects"},
+    {href:"#contact", label:"Contact"}
+]
